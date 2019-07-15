aab5277d5eb38ff2a8fe80c423c6468c1b4aa1c2
zhangxuan1918 <zhangxuan1918@gmail.com>
2019-07-15 21:55:17 +0200

    clean up example code

--- sample/tensorflow_2_0_py3/texture_3dmm.py
+++ sample/tensorflow_2_0_py3/texture_3dmm.py
@@ -13,64 +13,6 @@ from tf_3dmm.morphable_model.morphable_model import TfMorphableModel
 frame_width, frame_height = 450, 450
 
 
-def unit(vector):
-    return tf.convert_to_tensor(vector) / tf.norm(vector)
-
-
-def uvs_to_pixel_indices(uvs, texture_shape, mode='repeat'):
-    # Note that this assumes u = 0, v = 0 is at the top-left of the image -- different to OpenGL!
-
-    uvs = uvs[..., ::-1]  # change x, y coordinates to y, x indices
-    texture_shape = tf.cast(texture_shape, tf.float32)
-    if mode == 'repeat':
-        return uvs % 1. * texture_shape
-    elif mode == 'clamp':
-        return tf.clip_by_value(uvs, 0., 1.) * texture_shape
-    else:
-        raise NotImplementedError
-
-
-def sample_texture(texture, indices, mode='bilinear'):
-    if mode == 'nearest':
-
-        return tf.gather_nd(texture, tf.cast(indices, tf.int32))
-
-    elif mode == 'bilinear':
-
-        floor_indices = tf.floor(indices)
-        frac_indices = indices - floor_indices
-        floor_indices = tf.cast(floor_indices, tf.int32)
-
-        neighbours = tf.gather_nd(
-            texture,
-            tf.stack([
-                floor_indices,
-                floor_indices + [0, 1],
-                floor_indices + [1, 0],
-                floor_indices + [1, 1]
-            ]),
-        )
-        top_left, top_right, bottom_left, bottom_right = tf.unstack(neighbours)
-
-        return \
-            top_left * (1. - frac_indices[..., 1:]) * (1. - frac_indices[..., :1]) + \
-            top_right * frac_indices[..., 1:] * (1. - frac_indices[..., :1]) + \
-            bottom_left * (1. - frac_indices[..., 1:]) * frac_indices[..., :1] + \
-            bottom_right * frac_indices[..., 1:] * frac_indices[..., :1]
-
-    else:
-
-        raise NotImplementedError
-
-
-def process_uv(uv_coords, uv_h=256, uv_w=256):
-    uv_coords[:, 0] = uv_coords[:, 0] * (uv_w - 1)
-    uv_coords[:, 1] = uv_coords[:, 1] * (uv_h - 1)
-    uv_coords[:, 1] = uv_h - uv_coords[:, 1] - 1
-    uv_coords = np.hstack((uv_coords, np.zeros((uv_coords.shape[0], 1))))  # add z
-    return np.asarray(uv_coords, np.float32)
-
-
 def main():
     tf_bfm = TfMorphableModel('../../examples/Data/BFM/Out/BFM.mat')
     # --load mesh data
