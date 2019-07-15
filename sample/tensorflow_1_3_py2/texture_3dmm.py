fix bug in rotation matrix

sample/tensorflow_1_3_py2/texture_3dmm.py
@@ -94,7 +94,7 @@ def main():
     norm = mesh.render.generate_vertex_norm(vertices=vertices, triangles=bfm.triangles,
                                             nver=bfm.nver, ntri=bfm.ntri)
 
-    colors = bfm.generate_tex_color_xuan(tex, cp, ip, norm.numpy())
+    colors = bfm.generate_tex_color_xuan(tex, cp, ip, norm)
     colors = np.clip(colors / 255., 0., 1.)
     colors = np.asarray(colors, dtype=np.float32)
 

sample/tensorflow_2_0_py3/texture_3dmm.py
@@ -7,8 +7,9 @@ import scipy.io as sio
 import tensorflow as tf
 from dirt import lighting
 
-from face3d.morphable_model import MorphabelModel
-
+from tf_3dmm.mesh.transform import affine_transform
+from tf_3dmm.morphable_model.morphable_model import TfMorphableModel
+from tf_3dmm import mesh
 frame_width, frame_height = 450, 450
 
 
@@ -71,56 +72,51 @@ def process_uv(uv_coords, uv_h=256, uv_w=256):
 
 
 def main():
-    bfm = tfMorphabelModel('../../examples/Data/BFM/Out/BFM.mat')
+    tf_bfm = TfMorphableModel('../../examples/Data/BFM/Out/BFM.mat')
     # --load mesh data
     pic_name = 'IBUG_image_008_1_0'
     # pic_name = 'IBUG_image_014_01_2'
     mat_filename = '../../examples/Data/{0}.mat'.format(pic_name)
     mat_data = sio.loadmat(mat_filename)
-    sp = mat_data['Shape_Para']
-    ep = mat_data['Exp_Para']
+    sp = tf.constant(mat_data['Shape_Para'], dtype=tf.float32)
+    ep = tf.constant(mat_data['Exp_Para'], dtype=tf.float32)
 
-    vertices = bfm.generate_vertices(sp, ep)
-    triangles = bfm.triangles
+    vertices = tf_bfm.get_vertices(sp, ep)
+    triangles = tf_bfm.triangles
 
-    tp = mat_data['Tex_Para']
-    tex = bfm.generate_tex_xuan(tex_para=tp)
-    cp = mat_data['Color_Para']
-    ip = mat_data['Illum_Para']
+    tp = tf.constant(mat_data['Tex_Para'], dtype=tf.float32)
+    cp = tf.constant(mat_data['Color_Para'], dtype=tf.float32)
+    ip = tf.constant(mat_data['Illum_Para'], dtype=tf.float32)
 
-    import face3d.mesh_numpy as mesh
-    # norm = mesh.render.generate_vertex_norm(vertices=vertices, triangles=bfm.triangles,
-    #                                         nver=bfm.nver, ntri=bfm.ntri)
+    # import face3d.mesh_numpy as mesh
+    # norm = mesh.render.generate_vertex_norm(vertices=vertices, triangles=tf_bfm.triangles,
+    #                                         nver=tf_bfm.nver, ntri=tf_bfm.ntri)
     norm = lighting.vertex_normals(vertices, triangles)
-    colors = bfm.generate_tex_color_xuan(tex, cp, ip, norm)
-    colors = np.clip(colors / 255., 0., 1.)
-    colors = np.asarray(colors, dtype=np.float32)
+    colors = tf_bfm.get_vertex_colors(tp, cp, ip, -norm)
+    colors = tf.clip_by_value(colors / 255., 0., 1.)
 
-    # colors = colors / np.max(colors)
-
-    pp = mat_data['Pose_Para']
+    pp = tf.constant(mat_data['Pose_Para'], dtype=tf.float32)
     s = pp[0, 6]
-    # angles = [np.rad2deg(pp[0, 0]), np.rad2deg(pp[0, 1]), np.rad2deg(pp[0, 2])]
     angles = pp[0, 0:3]
-    # angles = [0, 0, 0]
     t = pp[0, 3:6]
 
-    transformed_vertices = bfm.transform_3ddfa(vertices, s, angles, t)
-    transformed_vertices[:, 0] = transformed_vertices[:, 0] * 2 / frame_width - 1
-    transformed_vertices[:, 1] = transformed_vertices[:, 1] * 2 / frame_height - 1
-    transformed_vertices[:, 2] = -transformed_vertices[:, 2] / np.max(np.abs(transformed_vertices[:, 2]))
-    transformed_vertices = np.asarray(transformed_vertices, dtype=np.float32)
+    transformed_vertices = affine_transform(vertices, s, angles, t)
+    transformed_vertices_x = transformed_vertices[:, 0] * 2 / frame_width - 1
+    transformed_vertices_y = transformed_vertices[:, 1] * 2 / frame_height - 1
+    transformed_vertices_z = -transformed_vertices[:, 2] / tf.reduce_max(tf.abs(transformed_vertices[:, 2]))
 
     # Convert vertices to homogeneous coordinates
     transformed_vertices = tf.concat([
-        transformed_vertices,
+        tf.expand_dims(transformed_vertices_x, axis=1),
+        tf.expand_dims(transformed_vertices_y, axis=1),
+        tf.expand_dims(transformed_vertices_z, axis=1),
         tf.ones_like(transformed_vertices[:, -1:])
     ], axis=1)
 
     # Render the G-buffer channels (mask, UVs, and normals at each pixel) needed for deferred shading
     image = dirt.rasterise(
         vertices=transformed_vertices,  # (24, 4)
-        faces=triangles.tolist(),  # [[]]
+        faces=triangles,  # [[]]
         vertex_colors=colors,
         background=tf.zeros([frame_height, frame_width, 3]),
         width=frame_width, height=frame_height, channels=3

tf_3dmm/mesh/transform.py
@@ -26,38 +26,38 @@ def angle_to_matrix(angles_rad, dtype=tf.float32):
 
     x, y, z = angles_rad[0], angles_rad[1], angles_rad[2]
     # rotation matrix on x axis
-    rotate_x = tf.constant(
+    rotate_x = tf.Variable(
         [
             [1, 0, 0],
-            [0, tf.math.cos(x), -tf.math.sin(x)],
-            [0, tf.math.sin(x), tf.math.cos(x)]
+            [0, tf.math.cos(x), tf.math.sin(x)],
+            [0, -tf.math.sin(x), tf.math.cos(x)]
         ],
         dtype=dtype
     )
 
     # rotation matrix on y axis
-    rotate_y = tf.constant(
+    rotate_y = tf.Variable(
         [
-            [tf.math.cos(y), 0, tf.math.sin(y)],
+            [tf.math.cos(y), 0, -tf.math.sin(y)],
             [0, 1, 0],
-            [-tf.math.sin(y), 0, tf.math.cos(y)]
+            [tf.math.sin(y), 0, tf.math.cos(y)]
         ],
         dtype=dtype
     )
 
     # rotation matrix on z axis
-    rotate_z = tf.constant(
+    rotate_z = tf.Variable(
         [
-            [tf.math.cos(z), -tf.math.sin(z), 0],
-            [tf.math.sin(z), tf.math.cos(z), 0],
+            [tf.math.cos(z), tf.math.sin(z), 0],
+            [-tf.math.sin(z), tf.math.cos(z), 0],
             [0, 0, 1]
         ],
         dtype=dtype
     )
 
     # rotation matrix
-    rotate_xyz = tf.linalg.matmul(rotate_z, tf.linalg.matmul(rotate_y, rotate_x))
-
+    # rotate_xyz = tf.linalg.matmul(rotate_z, tf.linalg.matmul(rotate_y, rotate_x))
+    rotate_xyz = tf.linalg.matmul(tf.linalg.matmul(rotate_x, rotate_y), rotate_z)
     return rotate_xyz
 
 

tf_3dmm/morphable_model/morphable_model.py
@@ -2,8 +2,6 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import numpy as np
-from PIL import Image
 import tensorflow as tf
 
 from tf_3dmm.morphable_model.morphable_model_util import load_BFM
@@ -75,7 +73,8 @@ class TfMorphableModel(object):
 
         assert is_tf_expression(shape_param) and is_tf_expression(exp_param)
 
-        vertices = self.shape_mu + tf.linalg.matmul(self.shape_pc, shape_param) + tf.linalg.matmul(self.exp_pc, exp_param)
+        vertices = self.shape_mu + tf.linalg.matmul(self.shape_pc, shape_param) + tf.linalg.matmul(self.exp_pc,
+                                                                                                   exp_param)
         vertices = tf.reshape(vertices, (self.n_vertices, 3))
 
         tf.debugging.assert_shapes({vertices: (self.n_vertices, 3)})
@@ -143,7 +142,9 @@ class TfMorphableModel(object):
         amb = tf.linalg.diag(illum_param[0, 0:3])
         dirt = tf.linalg.diag(illum_param[0, 3:6])
 
-        l = tf.Variable([tf.math.cos(thetal) * tf.math.sin(phil), tf.math.sin(thetal), tf.math.cos(thetal) * tf.math.cos(phil)], dtype=tf.float32)
+        l = tf.Variable(
+            [tf.math.cos(thetal) * tf.math.sin(phil), tf.math.sin(thetal), tf.math.cos(thetal) * tf.math.cos(phil)],
+            dtype=tf.float32)
         h = l + tf.constant([0, 0, 1], dtype=tf.float32)
         h = h / tf.sqrt(tf.reduce_sum(tf.square(h)))
 
@@ -190,10 +191,10 @@ class TfMorphableModel(object):
 
 
 if __name__ == '__main__':
-
     pic_name = 'IBUG_image_008_1_0'
     mat_filename = '../../examples/Data/{0}.mat'.format(pic_name)
     import scipy.io as sio
+
     mat_data = sio.loadmat(mat_filename)
 
     shape_param = tf.constant(mat_data['Shape_Para'], dtype=tf.float32)