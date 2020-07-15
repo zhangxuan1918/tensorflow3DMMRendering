d4baa9b7253a3c560b6fc8bbcebcdb7bdfdd5a13
zhangxuan1918 <zhangxuan1918@gmail.com>
2020-07-15 20:16:30 +0200

    support other expression; fix bugs in shape mean

--- example_render_2.py
+++ example_render_2.py
@@ -1,15 +1,45 @@
+import os
+
 import imageio
 import numpy as np
 import tensorflow as tf
-from example_utils import load_params
+from matplotlib.axes import Axes
+import matplotlib.pyplot as plt
+from example_utils import load_params, load_images
 from tf_3dmm.mesh.render import render_batch
 from tf_3dmm.morphable_model.morphable_model import TfMorphableModel
+
+
+def plot_image_w_lm(ax: Axes, resolution: int, image, lm):
+    ax.set_ylim(bottom=resolution, top=0)
+    ax.set_xlim(left=0, right=resolution)
+    ax.imshow(image, origin='upper')
+    ax.plot(lm[0, 0:17], lm[1, 0:17], marker='o', markersize=2, linestyle='-',
+            color='w', lw=2)
+    ax.plot(lm[0, 17:22], lm[1, 17:22], marker='o', markersize=2, linestyle='-',
+            color='w', lw=2)
+    ax.plot(lm[0, 22:27], lm[1, 22:27], marker='o', markersize=2, linestyle='-',
+            color='w', lw=2)
+    ax.plot(lm[0, 27:31], lm[1, 27:31], marker='o', markersize=2, linestyle='-',
+            color='w', lw=2)
+    ax.plot(lm[0, 31:36], lm[1, 31:36], marker='o', markersize=2, linestyle='-',
+            color='w', lw=2)
+    ax.plot(lm[0, 36:42], lm[1, 36:42], marker='o', markersize=2, linestyle='-',
+            color='w', lw=2)
+    ax.plot(lm[0, 42:48], lm[1, 42:48], marker='o', markersize=2, linestyle='-',
+            color='w', lw=2)
+    ax.plot(lm[0, 48:60], lm[1, 48:60], marker='o', markersize=2, linestyle='-',
+            color='w', lw=2)
+    ax.plot(lm[0, 60:68], lm[1, 60:68], marker='o', markersize=2, linestyle='-',
+            color='w', lw=2)
 
 
 def example_render_batch2(pic_names: list, tf_bfm: TfMorphableModel, save_to_folder: str, n_tex_para:int):
     batch_size = len(pic_names)
 
-    shape_param_batch, exp_param_batch, tex_param_batch, color_param_batch, illum_param_batch, pose_param_batch = \
+    images_orignal = load_images(pic_names, '/opt/project/examples/Data/300W_LP/')
+
+    shape_param_batch, exp_param_batch, tex_param_batch, color_param_batch, illum_param_batch, pose_param_batch, lm_batch = \
         load_params(pic_names=pic_names, n_tex_para=n_tex_para)
 
     # pose_param: [batch, n_pose_param]
@@ -25,8 +55,9 @@ def example_render_batch2(pic_names: list, tf_bfm: TfMorphableModel, save_to_fol
     color_param_batch = tf.squeeze(color_param_batch)
     illum_param_batch = tf.squeeze(illum_param_batch)
     pose_param_batch = tf.squeeze(pose_param_batch)
+    lm_rended = tf_bfm.get_landmarks(shape_param_batch, exp_param_batch, pose_param_batch, batch_size, 450, is_2d=True, is_plot=True)
 
-    images = render_batch(
+    images_rendered = render_batch(
         pose_param=pose_param_batch,
         shape_param=shape_param_batch,
         exp_param=exp_param_batch,
@@ -37,15 +68,22 @@ def example_render_batch2(pic_names: list, tf_bfm: TfMorphableModel, save_to_fol
         frame_width=450,
         tf_bfm=tf_bfm,
         batch_size=batch_size
-    )
+    ).numpy().astype(np.uint8)
 
+    # for i, pic_name in enumerate(pic_names):
+    #     imageio.imsave('{folder}/rendered_{pic}.jpg'.format(folder=save_to_folder, pic=pic_name), images[i, :, :, :].numpy().astype(np.uint8))
     for i, pic_name in enumerate(pic_names):
-        imageio.imsave('{folder}/rendered_{pic}.jpg'.format(folder=save_to_folder, pic=pic_name), images[i, :, :, :].numpy().astype(np.uint8))
+        fig = plt.figure()
+        ax = fig.add_subplot(1, 2, 1)
+        plot_image_w_lm(ax, 450, images_orignal[i], lm_batch[i])
+        ax = fig.add_subplot(1, 2, 2)
+        plot_image_w_lm(ax, 450, images_rendered[i], lm_rended[i])
+        plt.savefig(os.path.join(save_to_folder, pic_name))
 
 
 if __name__ == '__main__':
     n_tex_para = 40
     tf_bfm = TfMorphableModel(model_path='./examples/Data/BFM/Out/BFM.mat', n_tex_para=n_tex_para)
     save_rendered_to = './output/render_batch2/'
-    pic_names = ['image00002', 'IBUG_image_014_01_2', 'AFW_134212_1_0', 'IBUG_image_008_1_0']
+    pic_names = ['IBUG_image_014_01_2', 'AFW_134212_1_0', 'IBUG_image_008_1_0']
     example_render_batch2(pic_names=pic_names, tf_bfm=tf_bfm, save_to_folder=save_rendered_to, n_tex_para=n_tex_para)
--- example_utils.py
+++ example_utils.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 import scipy.io as sio
 
 
-def load_params(pic_names, n_tex_para, data_folder='./examples/Data/'):
+def load_params(pic_names, n_tex_para, data_folder='./examples/Data/300W_LP'):
 
     # --load mesh data
     shape_param_batch = []
@@ -14,6 +14,7 @@ def load_params(pic_names, n_tex_para, data_folder='./examples/Data/'):
     color_param_batch = []
     illum_param_batch = []
     pose_param_batch = []
+    lm_batch = []
 
     for pic_name in pic_names:
         mat_filename = os.path.join(data_folder, '{0}.mat'.format(pic_name))
@@ -25,6 +26,7 @@ def load_params(pic_names, n_tex_para, data_folder='./examples/Data/'):
         color_param_batch.append(tf.constant(mat_data['Color_Para'], dtype=tf.float32))
         illum_param_batch.append(tf.constant(mat_data['Illum_Para'], dtype=tf.float32))
         pose_param_batch.append(tf.constant(mat_data['Pose_Para'], dtype=tf.float32))
+        lm_batch.append(mat_data['pt2d'])
 
     shape_param_batch = tf.stack(shape_param_batch, axis=0)
     exp_param_batch = tf.stack(exp_param_batch, axis=0)
@@ -32,8 +34,39 @@ def load_params(pic_names, n_tex_para, data_folder='./examples/Data/'):
     color_param_batch = tf.stack(color_param_batch, axis=0)
     illum_param_batch = tf.stack(illum_param_batch, axis=0)
     pose_param_batch = tf.stack(pose_param_batch, axis=0)
+    lm_batch = tf.stack(lm_batch, axis=0)
+    return shape_param_batch, exp_param_batch, tex_param_batch, color_param_batch, illum_param_batch, pose_param_batch, lm_batch
 
-    return shape_param_batch, exp_param_batch, tex_param_batch, color_param_batch, illum_param_batch, pose_param_batch
+
+def load_params_80k(pic_names, data_folder='/opt/project//examples/Data/80k'):
+
+    # --load mesh data
+    shape_param_batch = []
+    exp_param_batch = []
+    pose_param_batch = []
+
+    for pic_name in pic_names:
+        txt_filename = os.path.join(data_folder, '{0}.txt'.format(pic_name))
+        with open(txt_filename, 'r') as f:
+            # read shape parameters
+            row = f.readline()
+            param_shape = [float(v.strip()) for v in row.split()] # dim: 100
+            # read expression parameters
+            row = f.readline()
+            param_exp = [float(v.strip()) for v in row.split()] # dim: 79
+            # read pose parameters
+            row = f.readline()
+            param_pose = [float(v.strip()) for v in row.split()] # dim: 6
+
+        shape_param_batch.append(tf.constant(param_shape, dtype=tf.float32))
+        exp_param_batch.append(tf.constant(param_exp, dtype=tf.float32))
+        pose_param_batch.append(tf.constant(param_pose, dtype=tf.float32))
+
+    shape_param_batch = tf.stack(shape_param_batch, axis=0)
+    exp_param_batch = tf.stack(exp_param_batch, axis=0)
+    pose_param_batch = tf.stack(pose_param_batch, axis=0)
+
+    return shape_param_batch, exp_param_batch, pose_param_batch
 
 
 def load_images(pic_names, folder):
